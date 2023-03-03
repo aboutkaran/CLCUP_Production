19f7a7c9d885b0f0b1cdf9610131198b1a361b08
aboutkaran <karanghai528@gmail.com>
2023-03-03 15:31:42 +0530

    some changes

--- app/views.py
+++ app/views.py
@@ -129,9 +129,9 @@ def worlker_list(request,id):
     monthly = Booking.objects.get(employer_id__admin = request.user.id)
     booking=Employer.objects.get(admin=request.user.id)
     count=monthly.no_of_worker
-    monthly_offered=monthly.sallery_offerd/float(count)
+    monthly_offered=monthly.sallery_offerd
     depttype=emp_gov.section
-    member = Member.objects.filter(Q(role_id_id=id) | Q(job_seeker=depttype))
+    member = Member.objects.filter(Q(role_id_id=id) & Q(job_seeker=depttype))
     membercount=member.count()
     
     return render(request,'demo.html',{'member':member,'booking':booking,'mem':mem,'emp_gov':emp_gov,'count':count,'monthly_offered':monthly_offered,'membercount':membercount,'id':id})      
